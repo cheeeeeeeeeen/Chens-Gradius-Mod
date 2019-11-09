763e11c74938e7759b82386b972749687a5f3813
cheeeeeeeeeen <blancfaye7@gmail.com>
2019-11-09 20:55:22 +0800

    Fix #149: Organize Options Rule List

---
+++ OptionRules.cs
@@ -0,0 +1,115 @@
+﻿using ChensGradiusMod.Projectiles;
+using System.Collections.Generic;
+using Terraria.ID;
+
+namespace ChensGradiusMod
+{
+  public static class OptionRules
+  {
+    private static List<int> VanillaRules = new List<int>()
+    {
+      ProjectileID.CrystalVileShardHead, ProjectileID.CrystalVileShardShaft, ProjectileID.VilethornBase, ProjectileID.VilethornTip,
+      ProjectileID.Bee, ProjectileID.GiantBee, ProjectileID.RainbowBack, ProjectileID.CrystalPulse2, ProjectileID.ToxicCloud,
+      ProjectileID.ToxicCloud2, ProjectileID.ToxicCloud3, ProjectileID.IceBlock, ProjectileID.CrystalShard, ProjectileID.HallowStar,
+      ProjectileID.RainFriendly, ProjectileID.BloodRain, ProjectileID.FlowerPowPetal, ProjectileID.TinyEater,
+      ProjectileID.NorthPoleSnowflake, ProjectileID.FlaironBubble, ProjectileID.TerrarianBeam, ProjectileID.VortexBeater,
+      ProjectileID.NebulaArcanumSubshot, ProjectileID.NebulaArcanumExplosionShot, ProjectileID.NebulaArcanumExplosionShotShard,
+      ProjectileID.MoonlordArrowTrail, ProjectileID.DD2PhoenixBow, ProjectileID.MonkStaffT3_AltShot, ProjectileID.MonkStaffT3,
+      ProjectileID.MonkStaffT3_Alt, ProjectileID.Electrosphere, ProjectileID.Xenopopper, ProjectileID.MolotovFire,
+      ProjectileID.MolotovFire2, ProjectileID.MolotovFire3, ProjectileID.NettleBurstLeft, ProjectileID.NettleBurstEnd,
+      ProjectileID.StyngerShrapnel, ProjectileID.SeedlerThorn, ProjectileID.DD2BetsyArrow, ProjectileID.PhantasmArrow,
+      ProjectileID.Phantasm, ProjectileID.VortexLightning
+    };
+
+    private static List<AlienProjectile> LocalModRules = new List<AlienProjectile>()
+    {
+      new AlienProjectile("CrystiliumMod", "DiamondExplosion"), new AlienProjectile("CrystiliumMod", "DiamondBomb"),
+      new AlienProjectile("CrystiliumMod", "Shatter1"), new AlienProjectile("CrystiliumMod", "Shatter2"),
+      new AlienProjectile("CrystiliumMod", "Shatter3"),
+
+      new AlienProjectile("CalamityMod", "Judgement"), new AlienProjectile("CalamityMod", "Whiterain"),
+      new AlienProjectile("CalamityMod", "WhiteOrb"), new AlienProjectile("CalamityMod", "PlagueExplosionFriendly"),
+      new AlienProjectile("CalamityMod", "BeamingBolt2"), new AlienProjectile("CalamityMod", "InfernalBlade2"),
+      new AlienProjectile("CalamityMod", "FlameBeamTip"), new AlienProjectile("CalamityMod", "FlameBeamTip2"),
+      new AlienProjectile("CalamityMod", "BlueBubble"), new AlienProjectile("CalamityMod", "Shockblast"),
+      new AlienProjectile("CalamityMod", "ClamorRifleProjSplit"), new AlienProjectile("CalamityMod", "GleamingBolt2"),
+      new AlienProjectile("CalamityMod", "SandyWaifuShark"), new AlienProjectile("CalamityMod", "ChargedBlast2"),
+      new AlienProjectile("CalamityMod", "FuckYou"), new AlienProjectile("CalamityMod", "FossilShard"),
+      new AlienProjectile("CalamityMod", "SHPExplosion"), new AlienProjectile("CalamityMod", "OMGWTH"),
+      new AlienProjectile("CalamityMod", "BrimstoneBeam2"), new AlienProjectile("CalamityMod", "BrimstoneBeam3"),
+      new AlienProjectile("CalamityMod", "BrimstoneBeam4"), new AlienProjectile("CalamityMod", "BrimstoneBeam5"),
+      new AlienProjectile("CalamityMod", "WaterStream2"), new AlienProjectile("CalamityMod", "Shaderain"),
+      new AlienProjectile("CalamityMod", "ChickenExplosion"), new AlienProjectile("CalamityMod", "AquashardSplit"),
+      new AlienProjectile("CalamityMod", "ManaBoltSmall"), new AlienProjectile("CalamityMod", "ManaBoltSmall2"),
+      new AlienProjectile("CalamityMod", "FungiOrb2"), new AlienProjectile("CalamityMod", "SerpentineHead"),
+      new AlienProjectile("CalamityMod", "SerpentineBody"), new AlienProjectile("CalamityMod", "SerpentineTail"),
+      new AlienProjectile("CalamityMod", "Flash"), new AlienProjectile("CalamityMod", "TerraBulletSplit"),
+      new AlienProjectile("CalamityMod", "TerraArrow2"), new AlienProjectile("CalamityMod", "VanquisherArrow2"),
+
+      new AlienProjectile("Bluemagic", "PuriumArrowTrail"),
+
+      new AlienProjectile("ThoriumMod", "TitaniumStaffPro2"), new AlienProjectile("ThoriumMod", "NightStar2"),
+      new AlienProjectile("ThoriumMod", "NightStarTrue2"), new AlienProjectile("ThoriumMod", "ShusSandstorm"),
+      new AlienProjectile("ThoriumMod", "Shroomy"), new AlienProjectile("ThoriumMod", "ChlorophyteCloud"),
+      new AlienProjectile("ThoriumMod", "AdamantiteStaffPro2"), new AlienProjectile("ThoriumMod", "EarthenSpawner"),
+      new AlienProjectile("ThoriumMod", "EarthenCascade"), new AlienProjectile("ThoriumMod", "EarthenSpawnerR"),
+      new AlienProjectile("ThoriumMod", "BlackHole"), new AlienProjectile("ThoriumMod", "LegionOrnamentShard"),
+      new AlienProjectile("ThoriumMod", "ClockWorkBombPro1"), new AlienProjectile("ThoriumMod", "ClockWorkBombPro2"),
+      new AlienProjectile("ThoriumMod", "ClockWorkBombPro3"), new AlienProjectile("ThoriumMod", "StalagmitePro"),
+      new AlienProjectile("ThoriumMod", "StalagmiteSpawnerR"),  new AlienProjectile("ThoriumMod", "StalagmiteSpike"),
+      new AlienProjectile("ThoriumMod", "ReactionFire"), new AlienProjectile("ThoriumMod", "HotPotFlame"),
+      new AlienProjectile("ThoriumMod", "NightStar2"), new AlienProjectile("ThoriumMod", "BoomPlasma"),
+      new AlienProjectile("ThoriumMod", "BoomNitrogen"), new AlienProjectile("ThoriumMod", "BoomCombustion"),
+      new AlienProjectile("ThoriumMod", "BoomAphrodisiac"), new AlienProjectile("ThoriumMod", "BoomCorrosive"),
+      new AlienProjectile("ThoriumMod", "DemonExplosion"), new AlienProjectile("ThoriumMod", "CrystalBalloonPro2"),
+      new AlienProjectile("ThoriumMod", "CorrupterBalloonPro2"), new AlienProjectile("ThoriumMod", "FesteringBalloonPro2"),
+      new AlienProjectile("ThoriumMod", "NightStaffPro2"), new AlienProjectile("ThoriumMod", "JellyPro"),
+      new AlienProjectile("ThoriumMod", "PalladiumBoltShatter"), new AlienProjectile("ThoriumMod", "BuriedArrowFireBoom"),
+      new AlienProjectile("ThoriumMod", "BuriedMagicPopPro"), new AlienProjectile("ThoriumMod", "MorelPoof"),
+      new AlienProjectile("ThoriumMod", "ChumShark"), new AlienProjectile("ThoriumMod", "FungalPopperPro2"),
+      new AlienProjectile("ThoriumMod", "SpikeBombBoom"), new AlienProjectile("ThoriumMod", "InfernoStaffPro2"),
+      new AlienProjectile("ThoriumMod", "HighTidePro2"), new AlienProjectile("ThoriumMod", "SeedBombPoof"),
+      new AlienProjectile("ThoriumMod", "SpineBreaker2"), new AlienProjectile("ThoriumMod", "SpineBreaker1Dummy"),
+      new AlienProjectile("ThoriumMod", "WebGunPro2"), new AlienProjectile("ThoriumMod", "MeteoriteClusterBombPro2"),
+      new AlienProjectile("ThoriumMod", "JunglesWrathPro2"), new AlienProjectile("ThoriumMod", "GorganGaze"),
+      new AlienProjectile("ThoriumMod", "Spud2"), new AlienProjectile("ThoriumMod", "OmniBoom"),
+      new AlienProjectile("ThoriumMod", "OmniBurst"), new AlienProjectile("ThoriumMod", "OmniBurstDamage"),
+
+      new AlienProjectile("SpiritMod", "FieryAura"), new AlienProjectile("SpiritMod", "DuskAura"),
+      new AlienProjectile("SpiritMod", "FireBolt"), new AlienProjectile("SpiritMod", "GeodeStaveProjectile"),
+      new AlienProjectile("SpiritMod", "AquaBolt"), new AlienProjectile("SpiritMod", "AquaFlareProj"),
+      new AlienProjectile("SpiritMod", "QuicksilverBolt1"), new AlienProjectile("SpiritMod", "BlueEmber"),
+      new AlienProjectile("SpiritMod", "ChaosB"), new AlienProjectile("SpiritMod", "CogW"),
+      new AlienProjectile("SpiritMod", "GrassPortal"), new AlienProjectile("SpiritMod", "AdamantiteStaffProj2"),
+      new AlienProjectile("SpiritMod", "BloodVessel"), new AlienProjectile("SpiritMod", "Blood3"),
+      new AlienProjectile("SpiritMod", "Fire"), new AlienProjectile("SpiritMod", "PhoenixMinion"),
+      new AlienProjectile("SpiritMod", "PrimeLaser"), new AlienProjectile("SpiritMod", "PrimeVice"),
+      new AlienProjectile("SpiritMod", "SkellyP"), new AlienProjectile("SpiritMod", "PrimeOther"),
+      new AlienProjectile("SpiritMod", "PrimeSaw"), new AlienProjectile("SpiritMod", "OrichHoming"),
+      new AlienProjectile("SpiritMod", "SpiritLinger"), new AlienProjectile("SpiritMod", "TitaniumStaffProj2"),
+      new AlienProjectile("SpiritMod", "IchorBomb"), new AlienProjectile("SpiritMod", "SpiritBoom"),
+      new AlienProjectile("SpiritMod", "GhostJellyBombProj"), new AlienProjectile("SpiritMod", "Blaze"),
+      new AlienProjectile("SpiritMod", "SoulRune"), new AlienProjectile("SpiritMod", "IchorP"),
+      new AlienProjectile("SpiritMod", "Fae"), new AlienProjectile("SpiritMod", "SadBeam"),
+      new AlienProjectile("SpiritMod", "StarTrail1"),  new AlienProjectile("SpiritMod", "GraniteShard1"),
+      new AlienProjectile("SpiritMod", "CoilMine"), new AlienProjectile("SpiritMod", "AbyssalSludge"),
+      new AlienProjectile("SpiritMod", "WitherShard3"), new AlienProjectile("SpiritMod", "HarpyFeather")
+    };
+
+    public static bool IsBanned(int pType) => VanillaCheck(pType) || LocalModCheck(pType);
+
+    public static bool IsAllowed(int pType) => !IsBanned(pType);
+
+    private static bool VanillaCheck(int pType) => VanillaRules.Contains(pType);
+
+    private static bool LocalModCheck(int pType)
+    {
+      foreach (AlienProjectile ap in LocalModRules)
+      {
+        if (ap.CheckType(pType)) return true;
+      };
+
+      return false;
+    }
+  }
+}
--- Projectiles/Options/OptionBaseObject.cs
+++ Projectiles/Options/OptionBaseObject.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace ChensGradiusMod.Projectiles.Options
@@ -14,7 +13,6 @@ namespace ChensGradiusMod.Projectiles.Options
     protected const int KeepAlive = 2;
 
     private const int MaxBuffer = 300;
-
     private List<int> playerAlreadyProducedProjectiles = new List<int>();
     private List<int> projectilesToProduce = new List<int>();
     private bool isSpawning = true;
@@ -67,7 +65,7 @@ namespace ChensGradiusMod.Projectiles.Options
           for (int i = 0; i < Main.maxProjectiles; i++)
           {
             Projectile p = Main.projectile[i];
-            if (p.active && FollowsRules(p) && IsNotAYoyo(p) && IsNotProducedYet(i) && !p.hostile && p.friendly &&
+            if (p.active && OptionRules.IsAllowed(p.type) && IsNotAYoyo(p) && IsNotProducedYet(i) && !p.hostile && p.friendly &&
                 !p.npcProj && GradiusHelper.CanDamage(p) && IsAbleToCrit(p) && !p.minion && !p.trap && IsSameOwner(p))
             {
               projectilesToProduce.Add(i);
@@ -180,220 +178,5 @@ namespace ChensGradiusMod.Projectiles.Options
     }
 
     private bool IsNotAYoyo(Projectile p) => p.aiStyle != 99;
-
-    private bool FollowsRules(Projectile p) => VanillaRules(p) && ModRules(p);
-
-    private bool VanillaRules(Projectile p)
-    {
-      return p.type != ProjectileID.CrystalVileShardHead &&
-             p.type != ProjectileID.CrystalVileShardShaft &&
-             p.type != ProjectileID.VilethornBase &&
-             p.type != ProjectileID.VilethornTip &&
-             p.type != ProjectileID.Bee &&
-             p.type != ProjectileID.GiantBee &&
-             p.type != ProjectileID.RainbowBack &&
-             p.type != ProjectileID.CrystalPulse2 &&
-             p.type != ProjectileID.ToxicCloud &&
-             p.type != ProjectileID.ToxicCloud2 &&
-             p.type != ProjectileID.ToxicCloud3 &&
-             p.type != ProjectileID.IceBlock &&
-             p.type != ProjectileID.CrystalShard &&
-             p.type != ProjectileID.HallowStar &&
-             p.type != ProjectileID.RainFriendly &&
-             p.type != ProjectileID.BloodRain &&
-             p.type != ProjectileID.FlowerPowPetal &&
-             p.type != ProjectileID.TinyEater &&
-             p.type != ProjectileID.NorthPoleSnowflake &&
-             p.type != ProjectileID.FlaironBubble &&
-             p.type != ProjectileID.TerrarianBeam &&
-             p.type != ProjectileID.VortexBeater && // This is not really a ban, but a fix.
-             p.type != ProjectileID.NebulaArcanumSubshot &&
-             p.type != ProjectileID.NebulaArcanumExplosionShot &&
-             p.type != ProjectileID.NebulaArcanumExplosionShotShard &&
-             p.type != ProjectileID.MoonlordArrowTrail &&
-             p.type != ProjectileID.DD2PhoenixBow && // This is not really a ban, but a fix.
-             p.type != ProjectileID.MonkStaffT3_AltShot &&
-             p.type != ProjectileID.MonkStaffT3 &&
-             p.type != ProjectileID.MonkStaffT3_Alt &&
-             p.type != ProjectileID.Electrosphere &&
-             p.type != ProjectileID.Xenopopper &&
-             p.type != ProjectileID.MolotovFire &&
-             p.type != ProjectileID.MolotovFire2 &&
-             p.type != ProjectileID.MolotovFire3 &&
-             p.type != ProjectileID.NettleBurstLeft &&
-             p.type != ProjectileID.NettleBurstRight &&
-             p.type != ProjectileID.NettleBurstEnd &&
-             p.type != ProjectileID.StyngerShrapnel &&
-             p.type != ProjectileID.SeedlerThorn &&
-             p.type != ProjectileID.DD2BetsyArrow &&
-             p.type != ProjectileID.PhantasmArrow &&
-             p.type != ProjectileID.Phantasm;
-    }
-
-    public bool ModRules(Projectile p)
-    {
-      Mod selectMod;
-      bool result = true;
-
-      selectMod = ModLoader.GetMod("CrystiliumMod");
-      if (selectMod != null)
-      {
-        result = result && p.type != selectMod.ProjectileType("DiamondExplosion")
-                        && p.type != selectMod.ProjectileType("DiamondBomb")
-                        && p.type != selectMod.ProjectileType("Shatter1")
-                        && p.type != selectMod.ProjectileType("Shatter2")
-                        && p.type != selectMod.ProjectileType("Shatter3");
-      }
-
-      selectMod = ModLoader.GetMod("CalamityMod");
-      if (selectMod != null)
-      {
-        result = result && p.type != selectMod.ProjectileType("Judgement")
-                        && p.type != selectMod.ProjectileType("Whiterain")
-                        && p.type != selectMod.ProjectileType("WhiteOrb")
-                        && p.type != selectMod.ProjectileType("PlagueExplosionFriendly")
-                        && p.type != selectMod.ProjectileType("BeamingBolt2")
-                        && p.type != selectMod.ProjectileType("InfernalBlade2")
-                        && p.type != selectMod.ProjectileType("FlameBeamTip")
-                        && p.type != selectMod.ProjectileType("FlameBeamTip2")
-                        && p.type != selectMod.ProjectileType("BlueBubble")
-                        && p.type != selectMod.ProjectileType("Shockblast")
-                        && p.type != selectMod.ProjectileType("ClamorRifleProjSplit")
-                        && p.type != selectMod.ProjectileType("GleamingBolt2")
-                        && p.type != selectMod.ProjectileType("SandyWaifuShark")
-                        && p.type != selectMod.ProjectileType("ChargedBlast2")
-                        && p.type != selectMod.ProjectileType("FuckYou")
-                        && p.type != selectMod.ProjectileType("FossilShard")
-                        && p.type != selectMod.ProjectileType("SHPExplosion")
-                        && p.type != selectMod.ProjectileType("OMGWTH")
-                        && p.type != selectMod.ProjectileType("BrimstoneBeam2")
-                        && p.type != selectMod.ProjectileType("BrimstoneBeam3")
-                        && p.type != selectMod.ProjectileType("BrimstoneBeam4")
-                        && p.type != selectMod.ProjectileType("BrimstoneBeam5")
-                        && p.type != selectMod.ProjectileType("WaterStream2")
-                        && p.type != selectMod.ProjectileType("Shaderain")
-                        && p.type != selectMod.ProjectileType("ChickenExplosion")
-                        && p.type != selectMod.ProjectileType("AquashardSplit")
-                        && p.type != selectMod.ProjectileType("ManaBoltSmall")
-                        && p.type != selectMod.ProjectileType("ManaBoltSmall2")
-                        && p.type != selectMod.ProjectileType("FungiOrb2")
-                        && p.type != selectMod.ProjectileType("SerpentineHead")
-                        && p.type != selectMod.ProjectileType("SerpentineBody")
-                        && p.type != selectMod.ProjectileType("SerpentineTail")
-                        && p.type != selectMod.ProjectileType("Flash")
-                        && p.type != selectMod.ProjectileType("TerraBulletSplit")
-                        && p.type != selectMod.ProjectileType("TerraArrow2")
-                        && p.type != selectMod.ProjectileType("VanquisherArrow2");
-      }
-
-      selectMod = ModLoader.GetMod("Bluemagic");
-      if (selectMod != null)
-      {
-        result = result && p.type != selectMod.ProjectileType("PuriumArrowTrail");
-      }
-
-      selectMod = ModLoader.GetMod("ThoriumMod");
-      if (selectMod != null)
-      {
-        result = result && p.type != selectMod.ProjectileType("TitaniumStaffPro2")
-                        && p.type != selectMod.ProjectileType("NightStar2")
-                        && p.type != selectMod.ProjectileType("NightStarTrue2")
-                        && p.type != selectMod.ProjectileType("ShusSandstorm")
-                        && p.type != selectMod.ProjectileType("Shroomy")
-                        && p.type != selectMod.ProjectileType("ChlorophyteCloud")
-                        && p.type != selectMod.ProjectileType("AdamantiteStaffPro2")
-                        && p.type != selectMod.ProjectileType("EarthenSpawner")
-                        && p.type != selectMod.ProjectileType("EarthenCascade")
-                        && p.type != selectMod.ProjectileType("EarthenSpawnerR")
-                        && p.type != selectMod.ProjectileType("BlackHole")
-                        && p.type != selectMod.ProjectileType("LegionOrnamentShard")
-                        && p.type != selectMod.ProjectileType("ClockWorkBombPro1")
-                        && p.type != selectMod.ProjectileType("ClockWorkBombPro2")
-                        && p.type != selectMod.ProjectileType("ClockWorkBombPro3")
-                        && p.type != selectMod.ProjectileType("StalagmitePro")
-                        && p.type != selectMod.ProjectileType("StalagmiteSpawnerR")
-                        && p.type != selectMod.ProjectileType("StalagmiteSpike")
-                        && p.type != selectMod.ProjectileType("ReactionFire")
-                        && p.type != selectMod.ProjectileType("HotPotFlame")
-                        && p.type != selectMod.ProjectileType("NightStar2")
-                        && p.type != selectMod.ProjectileType("BoomPlasma")
-                        && p.type != selectMod.ProjectileType("BoomNitrogen")
-                        && p.type != selectMod.ProjectileType("BoomCombustion")
-                        && p.type != selectMod.ProjectileType("BoomAphrodisiac")
-                        && p.type != selectMod.ProjectileType("BoomCorrosive")
-                        && p.type != selectMod.ProjectileType("DemonExplosion")
-                        && p.type != selectMod.ProjectileType("CrystalBalloonPro2")
-                        && p.type != selectMod.ProjectileType("CorrupterBalloonPro2")
-                        && p.type != selectMod.ProjectileType("FesteringBalloonPro2")
-                        && p.type != selectMod.ProjectileType("NightStaffPro2")
-                        && p.type != selectMod.ProjectileType("JellyPro")
-                        && p.type != selectMod.ProjectileType("PalladiumBoltShatter")
-                        && p.type != selectMod.ProjectileType("BuriedArrowFireBoom")
-                        && p.type != selectMod.ProjectileType("BuriedMagicPopPro")
-                        && p.type != selectMod.ProjectileType("MorelPoof")
-                        && p.type != selectMod.ProjectileType("ChumShark")
-                        && p.type != selectMod.ProjectileType("FungalPopperPro2")
-                        && p.type != selectMod.ProjectileType("SpikeBombBoom")
-                        && p.type != selectMod.ProjectileType("InfernoStaffPro2")
-                        && p.type != selectMod.ProjectileType("HighTidePro2")
-                        && p.type != selectMod.ProjectileType("SeedBombPoof")
-                        && p.type != selectMod.ProjectileType("SpineBreaker2")
-                        && p.type != selectMod.ProjectileType("SpineBreaker1Dummy")
-                        && p.type != selectMod.ProjectileType("WebGunPro2")
-                        && p.type != selectMod.ProjectileType("MeteoriteClusterBombPro2")
-                        && p.type != selectMod.ProjectileType("JunglesWrathPro2")
-                        && p.type != selectMod.ProjectileType("GorganGaze")
-                        && p.type != selectMod.ProjectileType("Spud2")
-                        && p.type != selectMod.ProjectileType("OmniBoom")
-                        && p.type != selectMod.ProjectileType("OmniBurst")
-                        && p.type != selectMod.ProjectileType("OmniBurstDamage");
-      }
-
-      selectMod = ModLoader.GetMod("SpiritMod");
-      if (selectMod != null)
-      {
-        result = result && p.type != ProjectileID.VortexLightning // Vanilla but no vanilla weapon uses this
-                        && p.type != selectMod.ProjectileType("FieryAura")
-                        && p.type != selectMod.ProjectileType("DuskAura")
-                        && p.type != selectMod.ProjectileType("FireBolt")
-                        && p.type != selectMod.ProjectileType("GeodeStaveProjectile")
-                        && p.type != selectMod.ProjectileType("AquaBolt")
-                        && p.type != selectMod.ProjectileType("AquaFlareProj")
-                        && p.type != selectMod.ProjectileType("QuicksilverBolt1")
-                        && p.type != selectMod.ProjectileType("BlueEmber")
-                        && p.type != selectMod.ProjectileType("ChaosB")
-                        && p.type != selectMod.ProjectileType("CogW")
-                        && p.type != selectMod.ProjectileType("GrassPortal")
-                        && p.type != selectMod.ProjectileType("AdamantiteStaffProj2")
-                        && p.type != selectMod.ProjectileType("BloodVessel")
-                        && p.type != selectMod.ProjectileType("Blood3")
-                        && p.type != selectMod.ProjectileType("Fire")
-                        && p.type != selectMod.ProjectileType("PhoenixMinion")
-                        && p.type != selectMod.ProjectileType("PrimeLaser")
-                        && p.type != selectMod.ProjectileType("PrimeVice")
-                        && p.type != selectMod.ProjectileType("SkellyP")
-                        && p.type != selectMod.ProjectileType("PrimeOther")
-                        && p.type != selectMod.ProjectileType("PrimeSaw")
-                        && p.type != selectMod.ProjectileType("OrichHoming")
-                        && p.type != selectMod.ProjectileType("SpiritLinger")
-                        && p.type != selectMod.ProjectileType("TitaniumStaffProj2")
-                        && p.type != selectMod.ProjectileType("IchorBomb")
-                        && p.type != selectMod.ProjectileType("SpiritBoom")
-                        && p.type != selectMod.ProjectileType("GhostJellyBombProj")
-                        && p.type != selectMod.ProjectileType("Blaze")
-                        && p.type != selectMod.ProjectileType("SoulRune")
-                        && p.type != selectMod.ProjectileType("IchorP")
-                        && p.type != selectMod.ProjectileType("Fae")
-                        && p.type != selectMod.ProjectileType("SadBeam")
-                        && p.type != selectMod.ProjectileType("StarTrail1")
-                        && p.type != selectMod.ProjectileType("GraniteShard1")
-                        && p.type != selectMod.ProjectileType("CoilMine")
-                        && p.type != selectMod.ProjectileType("AbyssalSludge")
-                        && p.type != selectMod.ProjectileType("WitherShard3")
-                        && p.type != selectMod.ProjectileType("HarpyFeather");
-      }
-
-      return result;
-    }
   }
 }
