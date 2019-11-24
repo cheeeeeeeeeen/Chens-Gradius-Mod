34a864944f83d6564d2c8d31039252befd81d550
cheeeeeeeeeen <blancfaye7@gmail.com>
2019-11-25 00:54:01 +0800

    Change rarity of boss banner

--- Items/Banners/BigCoreCustomBanner.cs
+++ Items/Banners/BigCoreCustomBanner.cs
@@ -5,6 +5,13 @@ namespace ChensGradiusMod.Items.Banners
 {
   public class BigCoreCustomBanner : ParentBanner
   {
+    public override void SetDefaults()
+    {
+      base.SetDefaults();
+
+      item.rare = 2;
+    }
+
     protected override int PartnerTile => ModContent.TileType<BigCoreCustomBannerTile>();
   }
 }
